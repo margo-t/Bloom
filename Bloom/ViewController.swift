0166bd2e917756c75e3f11b7ff76a9135f84cd96
Margarita Tereshchenkova <mtereshc@gmail.com>
2018-02-10 09:59:03 -0500

    Add ImagePicker and get the first guess with the model

--- Bloom/ViewController.swift
+++ Bloom/ViewController.swift
@@ -7,17 +7,70 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    
+    @IBOutlet weak var photoTaken: UIImageView!
+    
+    @IBAction func cameraButton(_ sender: Any) {
+
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            photoTaken.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model){(request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model Failed to process")
+            }
+            
+            print(result)
+            if let firstResult = result.first {
+                    self.navigationItem.title = firstResult.identifier
+                }
+            }
+            //print(result.first?.identifier)
+        
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print (error)
+        }
     }
 
 
